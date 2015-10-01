e7e440b4ea0f496acbe4e67e8e22176c0a28e08c
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-10-01 15:30:58 -0700

    Adds twitter object to server.js

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var express = require( 'express' );
 var app     = express();
 var http    = require( 'http' ).Server( app );
 var io      = require( 'socket.io' )( http );
+var Twit    = require( 'twit' );
 var port    = 3000;
+
+//Twitter object
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 //Home Route
 app.get( '/', function( req, res ) {
